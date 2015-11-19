11aa601ba2fd50bd2ce7764aee1cf2f970e074da
laurelfulford <laurelfulford@gmail.com>
2015-11-19 23:48:18 +0000

    Add Slick JS Library and styles to theme.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+( function( $ ) {
+
+	$('.theme-slider').slick( {
+		'autoplay': false,
+	} );
+
+} )( jQuery );
--- functions.php
+++ functions.php
@@ -115,11 +115,14 @@ add_action( 'widgets_init', 'components_widgets_init' );
 function components_scripts() {
 	wp_enqueue_style( 'components-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'components', get_template_directory_uri() . '/assets/js/main.js', array(), '20120206', true );
+	wp_enqueue_script( 'components', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery', 'components-slick'), '20120206', true );
 
 	wp_enqueue_script( 'components-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'components-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'components-slick', get_template_directory_uri() . '/assets/js/slick.js', array(), '20151119', true );
+
 
 	if ( wp_style_is( 'genericons', 'registered' ) ) {
 		wp_enqueue_style( 'genericons' );
