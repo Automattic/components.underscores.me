8c32faa68dc58a4d816acacdd2a47119e6d9fd62
laurelfulford <laurelfulford@gmail.com>
2015-11-20 11:36:21 +0000

    Swapping in simple keyframe animation for robot.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,30 @@
 ( function( $ ) {
 
 	$('.theme-slider').slick( {
-		'autoplay': true,
+		//'autoplay': true,
+	} );
+
+	$('.theme-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide ){
+
+		var nextBot = $(".slick-slide[data-slick-index=" + nextSlide + "] .theme-image");
+		$( nextBot ).addClass( 'hide-bot' );
+
 	} );
 
 	//On before slide change
-	$('.theme-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-		$(".slick-slide[data-slick-index=" + currentSlide + "]").siblings().removeClass('drop-bot');
-		$(".slick-slide[data-slick-index=" + currentSlide + "]").addClass('drop-bot');
+	$('.theme-slider').on('afterChange', function(event, slick, currentSlide){
+		//$(".slick-slide[data-slick-index=" + currentSlide + "]").siblings().removeClass('');
+
+		var currentBot = $(".slick-slide[data-slick-index=" + currentSlide + "] .theme-image");
+
+		var el = $( currentBot ),
+		newone = el.clone( true );
+
+		el.before( newone );
+		$(el).remove();
+
+		$( newone ).removeClass( 'hide-bot' );
+
 	} );
 
 	//
