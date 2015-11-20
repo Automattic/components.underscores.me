0ea91d5b07bb27039c51c9c4fb2b35a2cc03b090
laurelfulford <laurelfulford@gmail.com>
2015-11-20 10:18:41 +0000

    Adding rudimentary JS event and CSS animation for dropping robots.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,12 @@
 		'autoplay': true,
 	} );
 
+	//On before slide change
+	$('.theme-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+		console.log( currentSlide );
+		$(".slick-slide[data-slick-index=" + currentSlide + "]").siblings().removeClass('drop-bot');
+		$(".slick-slide[data-slick-index=" + currentSlide + "]").addClass('drop-bot');
+		$( currentSlide ).addClass('hey');
+	} );
+
 } )( jQuery );
