c6c7344ac7324769c08c80a9ec4bf2b9eb56d2c8
laurelfulford <laurelfulford@gmail.com>
2015-11-20 15:06:28 +0000

    Moving controls for slideshow up to title area of slides.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,61 @@
 ( function( $ ) {
 
-	$('.theme-slider').slick( {
-		//'autoplay': true,
-		appendArrows: '.slider-nav',
 
+	/**
+	 * Slideshow
+	 */
+
+	// Initalize Slick Slider
+	$( '.theme-slider' ).slick( {
+		arrows: false,
+		speed: 500,
 	} );
 
+	// Add prev/next arrows to individual slides
+	$( '.theme-slider .theme-text h3').append('<span class="next-type"><span class="screen-reader-text">Next</span></span>');
+	$( '.theme-slider .theme-text h3').prepend('<span class="prev-type"><span class="screen-reader-text">Previous</span></span>');
+
+	$('body').on('click', '.prev-type', function() {
+			$('.theme-slider').slick('slickNext');
+	});
+
+	$('body').on('click', '.next-type', function() {
+			$('.theme-slider').slick('slickPrev');
+	});
+
+
+
+	// toggle navigation for dropping robots
+
+	// run before slide changes
 	$('.theme-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide ){
 
 		var nextBot = $(".slick-slide[data-slick-index=" + nextSlide + "] .theme-image");
+		// add class to position next bot prior to animation
 		$( nextBot ).addClass( 'hide-bot' );
 
+		// add same to 'cloned' slide bots, since they are kind of out of the regular slide flow
 		$( ".slick-cloned.slick-slide .theme-image" ).addClass( 'hide-bot' );
 
+		// toggle class to turn on gears
 		$( '.slider-nav' ).addClass( 'gears-spinning' );
 
 	} );
 
-	//On before slide change
+	//Run after slide changes
 	$('.theme-slider').on('afterChange', function(event, slick, currentSlide){
 		var currentBot = $(".slick-slide[data-slick-index=" + currentSlide + "] .theme-image");
 
+		// recreate element to restart CSS3 animation
 		var el = $( currentBot ),
 		newone = el.clone( true );
-
 		el.before( newone );
 		$(el).remove();
 
+		// remove class that's hiding bot
 		$( newone ).removeClass( 'hide-bot' );
 
+		// toggle class to turn off gears
 		$( '.slider-nav' ).removeClass( 'gears-spinning' );
 
 	} );
