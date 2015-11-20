b4ac8d8365bdd7562f0750d85e0bf2f110920fe5
alaczek <ola@bodera.com>
2015-11-20 13:38:35 +0000

    Updating the copy.

--- front-page.php
+++ front-page.php
@@ -15,17 +15,17 @@ get_header(); ?>
 
 		<section id="intro" role="main">
 			<div class="wrap">
-				<p>Components is a library of shareable, reusable patterns for WordPress themes. Instead of starting from scratch, mix and match to build your own custom starter theme. Think of it as your scaffolding. We build the foundation and wire everything up—you move in and paint the walls.</p>
+				<p>Components is a library of shareable, reusable patterns for WordPress themes. Instead of starting from scratch, mix and match from a collection of pre-made components to build your own custom starter theme.</p>
 
 				<h2>Is Components right for me?</h2>
-				<p>Components is designed for WordPress theme enthusiasts of all levels. If you're just starting out, it'll get you started without needing to reinvent the wheel or build a lot of custom code. If you're an experienced theme developer, you'll find well-organised, easy-to use code that you can remix to your heart's delight!</p>
+				<p>If you're just starting out, it'll get you booted up without needing to reinvent the wheel or write a lot of custom code. If you're an experienced theme developer, you'll find well-organized, easy-to use code that you can remix to your heart's delight!</p>
 			</div><!-- .wrap -->
 		</section><!-- #intro -->
 
 		<section id="types" role="main">
 			<div class="wrap">
 				<h2>Let's get you a theme!</h2>
-				<p>We've got some pre-fab packages for five common theme types. If that's not quite what you're after, you can [download everything instead.](#download-all)</p>
+				<p>We've put together some pre-fab packages for five common theme types. If they don't suit your project, you can [download everything instead.]</p>
 				<a href="#">Download All</a>
 
 				<div class="theme-slider">
@@ -35,7 +35,7 @@ get_header(); ?>
 						</div>
 						<div class="theme-text">
 							<h3>Modern blog</h3>
-							<p>You think the world could be a little bit less cluttered. You like clean lines, simple shapes, and xxx. You want the focus to be on your content, not a lot of other stuff. With a single-column layout, a large featured image, and a slide-out panel for widgets and navigation, a modern blog is the perfect fit for you.</p>
+							<p>You think the world could be a little bit less cluttered. You like clean lines, simple shapes, and contemporary design. You want the focus to be on your content, not a lot of other stuff. With a single-column layout, a large featured image, and a slide-out panel for widgets and navigation, a modern blog is the perfect fit.</p>
 							<a href="#">Download Modern Blog Theme Package</a>
 						</div>
 					</div><!-- .theme-type -->
@@ -46,7 +46,7 @@ get_header(); ?>
 						</div>
 						<div class="theme-text">
 							<h3>Classic blog</h3>
-							<p>Maybe you prefer the classics. Austen. Dickens. AC/DC. A classic blog is your perfect match. Great for food blogs, xxx, or anyone else who has a lot of content they'd like to display in a sidebar, a classic blog features widgets in sidebar, just like you remembered.</p>
+							<p>Maybe you prefer the classics. Austen. Dickens. AC/DC. A classic blog is your perfect match. Great for food blogs, schools, or anyone else who has a lot of content to display in a sidebar, a classic blog features widgets in sidebar, just like you remember.</p>
 							<a href="#">Download Classic Blog Theme Package</a>
 						</div>
 					</div><!-- .theme-type -->
@@ -57,7 +57,7 @@ get_header(); ?>
 						</div>
 						<div class="theme-text">
 							<h3>Magazine</h3>
-							<p>Stop the presses! A magazine theme is ideal for showcasing content and images in a dynamic way. This theme features a front page template with a grid of featured images, and a two-column blog layout that relies on excerpts for some added interest.</p>
+							<p>Stop the presses! A magazine theme is ideal for showcasing content and images in a dynamic way. This theme features a front-page template with a grid of featured images, and a two-column blog layout that displays excerpts for added interest.</p>
 							<a href="#">Download Magazine Theme Package</a>
 						</div>
 					</div><!-- .theme-type -->
@@ -68,7 +68,7 @@ get_header(); ?>
 						</div>
 						<div class="theme-text">
 							<h3>Portfolio</h3>
-							<p>If you're the creative type, this is the theme package for you. Image-focused, the portfolio package uses a custom portfolio post type to easily keep your portfolio items separate from your posts. It uses a gridded portfolio layout, a simple one-column blog template, and a large featured image header.</p>
+							<p>If you're the creative type, this is the theme package for you. Image-focused, the portfolio layout uses a portfolio custom post type to easily keep your portfolio items separate from regular posts. It features a gridded portfolio layout, a simple one-column blog template, and a large featured image header.</p>
 							<a href="#">Download Portfolio Theme Package</a>
 						</div>
 					</div><!-- .theme-type -->
@@ -79,13 +79,16 @@ get_header(); ?>
 						</div>
 						<div class="theme-text">
 							<h3>Business</h3>
-							<p>You've got a million things to worry about; don't let your theme be another. A business starter theme comes with a front page template featuring a custom header, prominent testimonials, and a custom content area. Testimonials can be used throughout the theme to add authenticity to your business.</p>
+							<p>You've got a million things to worry about; don't let your theme be another. A business starter theme comes with a front page template featuring a custom header, prominent testimonials, and a custom content area. Testimonials can be displayed throughout the theme to add authenticity to your business.</p>
 							<a href="#">Download Business Theme Package</a>
 						</div>
 					</div><!-- .theme-type -->
 				</div><!-- .theme-slider -->
 				<div class="slider-nav"></div>
 
+				<h2>I want it all!</h2>
+				<p>Can't decide? Want to concoct your own starter theme? Here you go!</p>
+				<a href="#">Download everything</a>
 			</div><!-- .wrap -->
 		</section><!-- #types -->
 
@@ -96,11 +99,18 @@ get_header(); ?>
 				<ul>
 					<li>Design-agnostic layout patterns</li>
 					<li>Well-organised SCSS</li>
-					<li>Mobile first layouts</li>
+					<li>Mobile-first layouts</li>
 					<li>Mobile and desktop menus</li>
 				</ul>
 			</div><!-- .wrap -->
 		</section><!-- #extra-info -->
+
+		<section id="contribute" role="main">
+			<div class="wrap">
+				<h2>Want to contribute?</h2>
+				<p>Components is still a new project, and we're looking for your input! Have a great pattern you'd like to share? Want to add a new feature to your themes? Found a bug in the code? Head over to the [https://github.com/Automattic/theme-pattern-library](GitHub repo)!</p>
+			</div><!-- .wrap -->
+		</section><!-- #contribute -->
 
 	</div><!-- #primary -->
 
