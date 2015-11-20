2001bfbadc7446eeddc07736b5d6d8bb3ac29a75
laurelfulford <laurelfulford@gmail.com>
2015-11-20 11:59:18 +0000

    Fixing issue with robot dropping and looping. Tweaking robot alignment.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,14 @@
 		var nextBot = $(".slick-slide[data-slick-index=" + nextSlide + "] .theme-image");
 		$( nextBot ).addClass( 'hide-bot' );
 
+		console.log( 'next slide: ' + nextSlide );
+
+		$(".slick-cloned.slick-slide .theme-image").addClass( 'hide-bot' );
+
 	} );
 
 	//On before slide change
 	$('.theme-slider').on('afterChange', function(event, slick, currentSlide){
-		//$(".slick-slide[data-slick-index=" + currentSlide + "]").siblings().removeClass('');
-
 		var currentBot = $(".slick-slide[data-slick-index=" + currentSlide + "] .theme-image");
 
 		var el = $( currentBot ),
@@ -24,6 +26,8 @@
 		$(el).remove();
 
 		$( newone ).removeClass( 'hide-bot' );
+
+		console.log( 'current slide: ' + currentSlide );
 
 	} );
 
