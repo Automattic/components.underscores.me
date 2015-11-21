1f075fb412c46ae1912e582b728a14cfcdb5de10
laurelfulford <laurelfulford@gmail.com>
2015-11-21 15:12:01 +0000

    Updating 404 page and adding style; adding sad 404 robot.

--- 404.php
+++ 404.php
@@ -10,45 +10,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<section class="error-404 not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'components' ); ?></h1>
-	</header><!-- .page-header -->
+			<section class="error-404 not-found">
+				<div class="wrap">
+					<div class="content-404">
+						<header class="page-header">
+							<h2 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'components' ); ?></h2>
+						</header><!-- .page-header -->
 
-	<div class="page-content">
-		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'components' ); ?></p>
-
-		<?php get_search_form(); ?>
-
-		<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-		<?php if ( components_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-			<div class="widget widget_categories">
-				<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'components' ); ?></h2>
-				<ul>
-					<?php
-					wp_list_categories( array(
-						'orderby'    => 'count',
-						'order'      => 'DESC',
-						'show_count' => 1,
-						'title_li'   => '',
-						'number'     => 10,
-						) );
-					?>
-				</ul>
-			</div><!-- .widget -->
-		<?php endif; ?>
-
-		<?php
-			/* translators: %1$s: smiley */
-			$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'components' ), convert_smilies( ':)' ) ) . '</p>';
-			the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-		?>
-
-		<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-	</div><!-- .page-content -->
-</section><!-- .error-404 -->
+						<div class="page-content">
+							<a href="/"><?php esc_html_e( 'Head back home &raquo;', 'components' ); ?></a>
+						</div><!-- .page-content -->
+					</div>
+					<div class="image-404">
+						<?php echo file_get_contents( esc_url( get_template_directory_uri() ) . '/assets/img/robot-sad.svg' ); ?>
+					</div>
+				</div>
+			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
