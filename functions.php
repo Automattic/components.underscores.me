85272af7767b8716665ba565a03c6640d27927f9
laurelfulford <laurelfulford@gmail.com>
2016-02-19 11:32:11 -0800

    Removing a number of unused files and SVGs from the theme. It's not to say we won't need these files in the future as the site grows - it's just that they're not needed now, and their presence could cause confusion.

--- functions.php
+++ functions.php
@@ -157,16 +157,6 @@ function components_get_contributors() {
 }
 
 /**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
