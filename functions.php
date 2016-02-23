c12d45d01d727d3d5d034e87b9bf4bcaddbb2ff4
alaczek <ola@bodera.com>
2016-02-23 20:16:12 +0100

    Removing custom menus and navigation.js file..

--- functions.php
+++ functions.php
@@ -34,19 +34,6 @@ function components_setup() {
 	add_theme_support( 'title-tag' );
 
 	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'top' => esc_html__( 'Top Menu', 'components' ),
-		'social'  => esc_html__( 'Social Links Menu', 'components' ),
-	) );
-
-	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
 	 */
@@ -106,8 +93,6 @@ function components_scripts() {
 	wp_enqueue_style( 'components-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'components', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery' ), '20120206', true );
-
-	// wp_enqueue_script( 'components-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'components-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
