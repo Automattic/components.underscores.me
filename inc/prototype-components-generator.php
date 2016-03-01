ee5349aec1d2c63689cdad5f4731923ada1d8784
David A. Kennedy <me@davidakennedy.com>
2016-03-01 16:15:18 -0500

    Add a file to begin prototyping new generator

---
+++ inc/prototype-components-generator.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Components Generator
+ * Description: Generates themes based on Components by Automattic.
+ */
+
+class Components_Generator_Plugin {
+}
+new Components_Generator_Plugin;
