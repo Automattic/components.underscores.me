d7c7a8746b96707b905d4ad57d53692ee599c041
David A. Kennedy <me@davidakennedy.com>
2016-03-02 14:11:57 -0500

    Add parse_config function
    * Places data in JSON files in an array for later use.
    * Will process config files.

--- inc/prototype-components-generator.php
+++ inc/prototype-components-generator.php
@@ -5,5 +5,13 @@
  */
 
 class Components_Generator_Plugin {
+
+	/**
+	 * Places data in JSON files in an array for later use.
+	 */
+	function parse_config( $file ) {
+		$json = file_get_contents( $file );
+		$data = json_decode( $json, TRUE );
+	}
 }
 new Components_Generator_Plugin;
