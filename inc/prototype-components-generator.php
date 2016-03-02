9d908ed9a7bf5121f01cca28b4b31ba5b10bea7d
David A. Kennedy <me@davidakennedy.com>
2016-03-02 15:31:42 -0500

    Refine parse_config function
    * Prefix the function with `public` to declare the visibility of the method.
    * Instead of assigning `$data`, it’s best to return the value.

--- inc/prototype-components-generator.php
+++ inc/prototype-components-generator.php
@@ -9,9 +9,9 @@ class Components_Generator_Plugin {
 	/**
 	 * Places data in JSON files in an array for later use.
 	 */
-	function parse_config( $file ) {
+	public function parse_config( $file ) {
 		$json = file_get_contents( $file );
-		$data = json_decode( $json, TRUE );
+		return json_decode( $json, TRUE );
 	}
 }
 new Components_Generator_Plugin;
