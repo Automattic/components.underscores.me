a489a3fb33bdbf66cd6857de4ee9919a3c960b41
David A. Kennedy <me@davidakennedy.com>
2016-03-03 11:28:20 -0500

    Add get_theme_components function
    * Downloads theme components from Github.
    * Moves file to specified destination.
    * Creates new build directory.
    * Deletes unneeded zip files.

--- inc/prototype-components-generator.php
+++ inc/prototype-components-generator.php
@@ -14,6 +14,30 @@ class Components_Generator_Plugin {
 		return json_decode( $json, TRUE );
 	}
 
+	/**
+	 * This gets our zip from the Github repo.
+	 */
+	public function get_theme_components( $destination ) {
+		// Our Github repo zip URL.
+		$repo_url = esc_url_raw( 'https://codeload.github.com/Automattic/theme-components/zip/master' );
+		// Repo file name.
+		$repo_file_name = 'theme-components-master.zip';
+		// Our build directory.
+		$build_dir = get_stylesheet_directory() . '/build/';
+		if ( ! file_exists( $build_dir ) && ! is_dir( $build_dir ) ) {
+			mkdir( $build_dir,  0755 );
+		}
+		// Get our download.
+		$this->download_file( $repo_url, $repo_file_name );
+		// Copy the file to its new directory.
+		copy( ABSPATH . $repo_file_name, $destination . $repo_file_name );
+		// Unzip the file.
+		$this->unzip_file( $destination . $repo_file_name );
+		// Delete the unneeded files.
+		$this->delete_file( ABSPATH . $repo_file_name ); // Original download in root.
+		$this->delete_file( $destination . $repo_file_name ); // Zip file, after it's moved and unzipped.
+	}
+
 	// Utility functions: These help the generator do its work.
 	/**
 	 * This downloads a file at a URL.
