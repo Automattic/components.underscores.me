08ba8a5691e860efa71bc06e89d49eed7485f62e
Jeffrey Pearce <jeffikus@gmail.com>
2016-03-10 15:46:36 -0800

    Adds selection data to content file.

--- section-selection.php
+++ section-selection.php
@@ -1,20 +1,21 @@
 <section id="selection">
+
 	<div class="types">
 		<div class="svg-container">
 			<?php echo file_get_contents( get_template_directory() . '/assets/img/type-cart.svg' ); ?>
 		</div>
-		<h2>Types</h2>
-		<p>Lay the groundwork for your WordPress theme with our <strong>ready-made Types</strong>. Crafting an elegant artist's <strong>portfolio</strong>? An information-filled <strong>magazine</strong> theme or <strong>business</strong> site? How about a modern or classic <strong>blog</strong>? Choose the type that fits the bill and you'll be on your way in no time.
-</p>
+		<?php $selection_data = get_content_selection('types'); ?>
+		<h2><?php echo $selection_data['title']; ?></h2>
+		<?php echo $selection_data['content']; ?>
 		<a href="#theme-types-panel" class="toggle button">Choose a Type</a>
 	</div>
 	<div class="custom">
 		<div class="svg-container">
 			<?php echo file_get_contents( get_template_directory() . '/assets/img/toolbox.svg' ); ?>
 		</div>
-		<h2>Build Your Own</h2>
-		<p>Go the custom route and <strong>keep control</strong> over every element of your starter theme, picking-and-choosing only the components you need to create your <strong>perfect custom starter theme</strong>. You'll get solid, reliable code, with only the pieces your project needs.
-</p>
+		<?php $selection_data = get_content_selection('custom'); ?>
+		<h2><?php echo $selection_data['title']; ?></h2>
+		<?php echo $selection_data['content']; ?>
 		<a href="#custom-build-panel" class="toggle button">Configure</a>
 	</div>
 </section><!-- #selection -->
